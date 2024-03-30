3e470da89d2501f155e275c036a789c47bec3547
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-03-30 12:58:42 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,41 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        char ch = 't';
+        switch (ch) {
+            case 'a':
+                System.out.println("Character is vowel");
+                break;
+            case 'e':
+                System.out.println("Character is vowel");
+                break;
+            case 'i':
+                System.out.println("Character is vowel");
+                break;
+            case 'o':
+                System.out.println("Character is vowel");
+                break;
+            case 'u':
+                System.out.println("Character is vowel");
+                break;
+            case 'A':
+                System.out.println("Character is vowel");
+                break;
+            case 'E':
+                System.out.println("Character is vowel");
+                break;
+            case 'I':
+                System.out.println("Character is vowel");
+                break;
+            case 'O':
+                System.out.println("Character is vowel");
+                break;
+            case 'U':
+                System.out.println("Character is vowel");
+                break;
+
+            default:
+                System.out.println("Character is Consonant");
+                break;
+        }
+    }
+}
